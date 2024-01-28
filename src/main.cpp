af5398fff00ba80b6776b27b79005559c47f9d3a
Guillermo Valverde <guillermovalverde42@gmail.com>
2024-01-27 18:59:29 -0600

    clase 27 enero 2023

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,40 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
 #include <PubSubClient.h>
 #include "WiFiConexion.h"
 
 #define LED_PIN 2
 
-const char* ssid = "";
-const char* password = "";
+const char* ssid = "SALA 201";
+const char* password = "CG.PAGES";
 
 // Datos broker MQTT
-const char* mqttServer = "192.200.201.65";
-const char* mqttUsername = "";
-const char* mqttPassword = "";
+const char* mqttServer = "192.200.201.66";
+const char* mqttUsername = "memo";
+const char* mqttPassword = "password123";
 const int mqttPort = 1883;
 
-const char* topicLed = "guillermovalverdebaez/feeds/led";
-const char* topicMsg = "guillermovalverdebaez/feeds/msg";
+const char* topicLed = "esp32/led";
+const char* topicMsg = "esp32/msg";
 
-String message;
+const String baseURL = "https://api.openweathermap.org/data/2.5/weather?lat=19.1924201&lon=-96.1421051&units=metric&lang=es&appid=2bf5873e176fab2e510754fae8ffc595";
+unsigned long lastMillis = 0;
+
+float tempInMinValue = 0;
+float tempInMaxValue = 0;
+float humInMinValue = 0;
+float humInMaxValue = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-unsigned long lastMillis = 0;
-
 void reconnect( void );
 void callback(char* topic, byte* payload, unsigned int length);
+JSONVar httpGetRequest( String url );
+void setAlarm(String idAlarm, float min, float max);
+void updateAlarmState( float tempIn, float humIn );
 
 void setup( void ) {
   Serial.begin(9600);
@@ -46,17 +55,69 @@ void loop( void ) {
   }
   client.loop();
 
-  if(Serial.available()){
-    message = Serial.readString();
-    char messageChar[message.length() + 1];
+  if(millis() - lastMillis >= 5000) {
+    JSONVar owRespone = httpGetRequest(baseURL);
+    JSONVar data;
+    String msg;
 
-    message.toCharArray(messageChar, message.length() + 1);
-    client.publish(topicMsg, messageChar);
+    float tempIn = dht.readTemperature(); 
+    float humIn = dht.readHumidity();
+
+    data["Estao interior"]["Temp"] = tempIn;
+    data["Estao interior"]["Hume"] = humIn;
+    data["Estado exterior"]["Temp"] = JSON.stringify(owRespone["main"]["temp"]);
+    data["Estado exterior"]["Hume"] = JSON.stringify(owRespone["main"]["humidity"]);
+
+    msg = JSON.stringify(data);
+    char dataChar[msg.length() + 1];
+
+    msg.toCharArray(dataChar, msg.length() + 1);
+    client.publish("esp32/temp", dataChar);
+
+    lastMillis = millis();
   }
 
 }
 
+void updateAlarmState( float tempIn, float humIn ) {
+  if(tempInMinValue != 0 && tempInMaxValue != 0){
+    if(tempIn > tempInMaxValue){
+      // Activan alarma de temperatura interior alta
+    } else if(tempIn < tempInMinValue) {
+      // Activan alarma de temperatura interior muy baja
+    } else {
+      // Alarma desactivado
+    }
+  }
+
+  if(humInMinValue != 0 && humInMaxValue != 0){
+    if(humIn > humInMaxValue){
+      // Activan alarma de temperatura interior alta
+    } else if(humIn < humInMinValue) {
+      // Activan alarma de temperatura interior muy baja
+    } else {
+      // Alarma desactivado
+    }
+  }
+}
+
+void setAlarm(String idAlarm, float min, float max) {
+  if(idAlarm == "temp-in"){
+    tempInMinValue = min;
+    tempInMaxValue = max;
+  } else if(idAlarm == "temp-ex") {
+
+  } else if(idAlarm == "hum-in") {
+    humInMinValue = min;
+    humInMaxValue = max;
+    
+  } else if(idAlarm == "hum-ex") {
+    
+  }
+}
+
 void callback(char* topic, byte* payload, unsigned int length) {
+
   String payloadMsg;
 
   for(int i=0; i<length; i++){
@@ -64,11 +125,21 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
   Serial.println(payloadMsg);
 
-  if(String(topic) == topicLed){
-    if(payloadMsg == "on")
-      digitalWrite(LED_PIN, HIGH);
-    else
-      digitalWrite(LED_PIN, LOW);
+  if(String(topic) == "esp32/set-alarm"){
+    JSONVar newMsg = JSON.parse(payloadMsg);
+
+    String id = newMsg["ID"];
+    float min = JSON.stringify(newMsg["values"]["min"]).toFloat();
+    float max = JSON.stringify(newMsg["values"]["max"]).toFloat();
+
+    Serial.print("ID Alarma: ");
+    Serial.println(id);
+    Serial.print("min: ");
+    Serial.println(min);
+    Serial.print("max: ");
+    Serial.println(max);
+
+    setAlarm(id, min, max);
   }
 
 }
@@ -78,10 +149,9 @@ void reconnect( void ) {
   while (!client.connected()) {
     Serial.println("Intentando conexión a servidor MQTT...");
 
-    if (client.connect("esp32Guillermo", mqttUsername, mqttPassword)) {
+    if (client.connect("sgdfgsds", mqttUsername, mqttPassword)) {
       Serial.println("Conectado");
-      client.subscribe(topicLed);
-      client.subscribe("esp32/ibox");
+      client.subscribe("esp32/set-alarm");
     } else {
       Serial.print("Falló, rc=");
       Serial.print(client.state());
@@ -91,5 +161,39 @@ void reconnect( void ) {
   }
 
 }
+
+JSONVar httpGetRequest( String url ) {
+
+  int httpCode;
+  JSONVar jsonObject;
+  HTTPClient http;
+  
+  http.begin(url);
+  httpCode = http.GET();                                    
+  
+  if (httpCode > 0) {
+  
+    String jsonBuffer = http.getString();
+    // Serial.println(httpCode);
+    // Serial.println(jsonBuffer);
+
+    jsonObject = JSON.parse(jsonBuffer);
+  
+    // if (JSON.typeof(jsonObject) == "undefined") {
+    //   Serial.println("Fallo de conversion de JSON");
+    //   return;
+    // }
+
+  } else {
+    Serial.println("Error en la peticion");
+  }
+  
+  http.end();
+
+  return jsonObject;
+
+}
+
+
 
 
